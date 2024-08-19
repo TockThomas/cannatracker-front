1c80697a5d7ca4f43a3660c2c3d628d8d6ab9f09
Thomas <thomas.schleicher@ship-cheaper.com>
2024-08-19 21:35:25 +0200

    fix bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
-	import { link } from 'svelte/transition';
-	export let $auth = {
-		status: '',
-	}
-	import Login from './login/+page.svelte'
 </script>
 
-{#if $auth.status === 'loggedOut'}
-	<div class="w-full h-full flex items-center justify-around">
-		<Login />
-	</div>
+<div class="w-full h-full flex items-center justify-around">
+</div>
